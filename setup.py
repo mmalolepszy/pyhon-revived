3614920efdb29f05d83dca1ef0423d397b988a80
Michał Małolepszy <mmalolepszy@gmail.com>
2025-11-10 00:09:33 +0100

    Bump version v0.18.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pyhon-revived",
-    version="0.18.0",
+    version="0.18.1",
     author="Andre Basche, Michał Małolepszy",
     description="Control hOn devices with python",
     long_description=long_description,
